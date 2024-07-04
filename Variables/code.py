8d2d34561e7f2c96aaf083c92ed8e0a52247e433
Rajratan Wankhade <rajratan.wankhade.dcu@gmail.com>
2024-07-04 08:44:09 +0100

    Variables

---
+++ Variables/code.py
@@ -0,0 +1,23 @@
+#Int and Float
+
+X = 15  #int
+price = 9.9 #float
+discount = 0.2
+result = price * (1 - discount)
+print(result)
+
+# Strings are cha' inside double quotation or single quotation.
+name = "Raj"
+surname = " Ratan"
+print(name + surname)
+
+#
+a = 25
+b = a
+print(a)
+print(b)
+b = 17
+print(a)
+print(b)
+
+#
